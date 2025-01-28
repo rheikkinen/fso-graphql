c447ff5c7c05f6c64563fe4ff67c18b394887c59
Rasmus Heikkinen <rasmus.heikkinen@outlook.com>
2025-01-28 11:52:15 +0200

    Add query to get all authors
    Exercise 8.3

--- index.ts
+++ index.ts
@@ -101,10 +101,16 @@ const typeDefs = `
     genres: [String!]
   }
 
+  type Author {
+    name: String!
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `;
 
@@ -113,6 +119,14 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: (root, args) => {
+      return authors.map((author) => {
+        const bookCount = books.filter(
+          (book) => book.author === author.name
+        ).length;
+        return { name: author.name, bookCount };
+      });
+    },
   },
 };
 

c34a3b6cdcc594ae74f4792b70a7ba98ef7b4e89
Rasmus Heikkinen <rasmus.heikkinen@outlook.com>
2025-01-29 11:30:30 +0200

    Add mutation to update author's birth year
    Exercise 8.7

--- index.ts
+++ index.ts
@@ -122,6 +122,10 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `;
 
@@ -159,6 +163,16 @@ const resolvers = {
         authors = authors.concat(newAuthor);
       }
       return book;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((author) => author.name === args.name);
+      if (!author) return null;
+
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((author) =>
+        author.name === args.name ? updatedAuthor : author
+      );
+      return updatedAuthor;
     },
   },
 };

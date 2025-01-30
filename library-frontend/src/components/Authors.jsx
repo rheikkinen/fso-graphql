bb55fcd84a8762f16360d73f8b3cbdf67bbd1afa
Rasmus Heikkinen <rasmus.heikkinen@outlook.com>
2025-01-30 17:07:35 +0200

    Implement authors list view with Apollo client
    Exercise 8.8

--- library-frontend/src/components/Authors.jsx
+++ library-frontend/src/components/Authors.jsx
@@ -1,30 +1,38 @@
-const Authors = (props) => {
-  if (!props.show) {
-    return null
+import { useQuery } from '@apollo/client';
+import { ALL_AUTHORS } from '../queries';
+
+const Authors = ({ show }) => {
+  const result = useQuery(ALL_AUTHORS);
+
+  if (!show) {
+    return null;
   }
-  const authors = []
+
+  if (result.loading) return <>Loading authors...</>;
+
+  const { allAuthors: authors } = result.data;
 
   return (
     <div>
-      <h2>authors</h2>
+      <h2>Authors</h2>
       <table>
         <tbody>
           <tr>
             <th></th>
-            <th>born</th>
-            <th>books</th>
+            <th>Born</th>
+            <th>Books</th>
           </tr>
           {authors.map((a) => (
             <tr key={a.name}>
               <td>{a.name}</td>
-              <td>{a.born}</td>
+              <td>{a.born ?? 'N/A'}</td>
               <td>{a.bookCount}</td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Authors
+export default Authors;
